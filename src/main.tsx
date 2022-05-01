bd202f77f3283661bebf8862eecb302b5261b000
unknown <moeinfar.farnaz@gmail.com>
2022-05-01 12:31:30 +0430

    add structure | componenet & route

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.scss";
+import AboutUs from "./pages/AboutUs";
+import Search from "./pages/Search";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Search />} />
+        <Route path="search" element={<Search />} />
+        <Route path="about-us" element={<AboutUs />} />
+        <Route
+          path="*"
+          element={
+            <main className="p-4">
+              <p>There's nothing here: 404!</p>
+            </main>
+          }
+        />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function AboutUs() {
+  return <div className="p-4">About us :)</div>;
+}
